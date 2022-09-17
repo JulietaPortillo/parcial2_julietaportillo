fc78832c6f3120ec0deded20dae4dc80215a6341
JulietaPortillo <julie.portillo96@gmail.com>
2022-09-17 11:37:19 -0600

    Models & Migrate

---
+++ database/migrations/2022_09_17_172800_create_libros_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLibrosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('libros', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('detail');
+            $table->string('author');
+            $table->string('editorial');
+            $table->integer('pages');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('libros');
+    }
+}
